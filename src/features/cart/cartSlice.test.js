e302359efa894eea84971007cecfc4f592f7fd33
Vaggelis Agapiou <vaggap73@gmail.com>
2026-05-31 13:06:08 +0300

    Added happy case test for increaseItemQuantity function

--- src/features/cart/cartSlice.test.js
+++ src/features/cart/cartSlice.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import cartReducer, { addItem } from "./cartSlice";
+import cartReducer, { addItem, increaseItemQuantity } from "./cartSlice";
  
 
 const mockPizza = {
@@ -11,8 +11,16 @@ const mockPizza = {
 }
 
 test("addItem adds a pizza to the cart", () => {
-    const initalState = { cart: [] }
+    const initalState = { cart: [] };
     const newState = cartReducer(initalState, addItem(mockPizza));
 
     expect(newState.cart).toHaveLength(1);
 });
+
+test("increaseItemQuantity updates quantity & totalPrice", () => {
+    const initialState =  {cart: [mockPizza]};
+    const newState = cartReducer(initialState, increaseItemQuantity(1));
+
+    expect(newState.cart[0].quantity).toBe(2);
+    expect(newState.cart[0].totalPrice).toBe(24);
+})
